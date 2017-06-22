7ed9addcb53118e14f4240a814f167d16d4bbcf4
schoeu <schoeu89@gmail.com>
2017-06-22 16:03:09 +0800

    update dev name.

--- godev.sh
+++ godev.sh
@@ -4,8 +4,8 @@
 # Have fun.
 
 vpsList='
-alafe4 cp01-ala-fe-4.epc.baidu.com,
-alafeplat1 cp01-ala-fe-plat-1.epc.baidu.com
+alafe4 alafe4.test.com,
+alafeplat1 plat-1.epc.test.com
 '
 user="work"
 targetDev=$1
@@ -19,4 +19,5 @@ fi
 dev=$vpsList | tr ',' '\n' | sed 's/^ //' | grep $targetDev | cut -d" " -f2
 
 ssh $user@$dev
+
 
