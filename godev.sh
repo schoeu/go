09d6a38acc5ed3d251c175039ee96fd611128ae9
schoeu <schoeu89@gmail.com>
2017-06-22 23:25:50 +0800

    update godev

--- godev.sh
+++ godev.sh
@@ -8,16 +8,59 @@ alafe4 alafe4.test.com,
 alafeplat1 plat-1.epc.test.com
 '
 user="work"
-targetDev=$1
+# targetDev=$1
 
-if [ $# != 1 ]
-then
-    echo 'One argument.'
-    exit 1
-fi
 
-dev=$vpsList | tr ',' '\n' | sed 's/^ //' | grep $targetDev | cut -d" " -f2
+# Show the dev list.
+showList(){
+    echo $vpsList | tr ',' '\n' | sed 's/^ //'
+}
 
-ssh $user@$dev
+# Show help infos.
+showHelp(){
+    echo "Usage: go [-li]"
+    echo "       go alafe4"
+}
+
+# Login dev
+loginDev(){
+    echo $A
+    if [ $# != 1 ]
+    then
+        echo 'One argument.'
+        exit 1
+    fi
+
+    dev=$vpsList | tr ',' '\n' | sed 's/^ //' | grep $targetDev | cut -d" " -f2
+    echo "Connectting "$user@$dev
+    ssh $user@$dev
+}
+
+# Process arguments.
+while getopts lh: option
+do
+    echo $option
+    case "$option"
+    in
+        l)
+            showList
+            exit 1
+            ;;
+        \?|h)
+            showHelp
+            exit 1
+            ;;
+        *)
+            loginDev $OPTARG
+            exit 0
+            ;;
+    esac
+done
+
+
+
+
+
+
 
 
